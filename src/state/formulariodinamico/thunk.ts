522442b439cc82d8a978a564db384749b21345d6
this.fabio <this.fabio@outlook.cl>
2021-08-26 11:30:53 -0400

    Se agrega localResource   - Se agrega interface ILocalResource   - Se corrige interface IResource   - Se redirigen resources en redux apuntando a localResources   - Se agrega thunk que por medio de un resource genera el localResource
    Falta en Thunk crear localResource cuando es un objeto

--- src/state/formulariodinamico/thunk.ts
+++ src/state/formulariodinamico/thunk.ts
@@ -1,9 +1,16 @@
 import axios, { AxiosResponse } from 'axios';
 import { ThunkDispatch } from 'redux-thunk';
 import Config from 'react-native-config';
-import { saveFormulario } from './actions';
-import { IFormulario } from 'types/formulariodinamico';
-import { SaveFormularioAction, SaveFormularioAsync, SaveFormularioAsyncThunk } from './types';
+import { saveFormulario, saveResource } from './actions';
+import { IFormulario, ILocalResource, IResource } from 'types/formulariodinamico';
+import {
+  SaveFormularioAction,
+  SaveFormularioAsync,
+  SaveFormularioAsyncThunk,
+  SaveLocalResourceAsync,
+  SaveLocalResourceAsyncThunk,
+  SaveResourceAction
+} from './types';
 
 export const saveFormulariosAsync: SaveFormularioAsync = (): SaveFormularioAsyncThunk => {
   return async (dispatch: ThunkDispatch<{}, {}, SaveFormularioAction>): Promise<void> => {
@@ -20,6 +27,32 @@ export const saveFormulariosAsync: SaveFormularioAsync = (): SaveFormularioAsync
         reject(error);
       } finally {
         resolve();
+      }
+    });
+  };
+};
+
+export const saveLocalResourceAsync: SaveLocalResourceAsync = (resource: IResource): SaveLocalResourceAsyncThunk => {
+  return async (dispatch: ThunkDispatch<{}, {}, SaveResourceAction>): Promise<ILocalResource> => {
+    return new Promise<ILocalResource>(async (resolve, reject) => {
+      try {
+        if (resource.type === 'api') {
+          const response: AxiosResponse<any> = await axios({
+            url: resource.url,
+            method: resource.method,
+            data: resource.body
+          });
+
+          dispatch(saveResource({
+            url: resource.url,
+            type: resource.type,
+            method: resource.method,
+            body: resource.body,
+            localData: response.data
+          }));
+        }
+      } catch (error) {
+        reject(error);
       }
     });
   };
