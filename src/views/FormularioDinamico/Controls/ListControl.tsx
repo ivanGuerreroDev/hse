9c486d2cd317093694eb6f009fc3bf126eb48dc5
this.fabio <this.fabio@outlook.cl>
2021-09-18 22:55:21 -0300

    Se actualiza control lista: Función total: tipo de dato: string y objetos Modo de selección: simple y multiple

--- src/views/FormularioDinamico/Controls/ListControl.tsx
+++ src/views/FormularioDinamico/Controls/ListControl.tsx
@@ -1,28 +1,69 @@
 import React from 'react';
 import ControlComponent, { Props } from '../ControlComponent';
 import { FlatList, Text, TextInput, View } from 'react-native';
+import { Icon } from 'react-native-elements';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import _ from 'lodash';
 
 type ListControlModalProps = {
   data: Array<any>;
   onSuccess: (output: any) => void;
   selectedData: any;
   displayField?: string;
+  multiple?: boolean;
 };
 
 class ListControlModal extends React.Component<ListControlModalProps> {
-  state = {
-    searchText: ''
+  state: {
+    searchText: string,
+    selectedData: Array<any>
+  } = {
+    searchText: '',
+    selectedData: []
   };
 
+  constructor(props: ListControlModalProps) {
+    super(props);
+
+    if (props.selectedData)
+      this.state.selectedData = props.multiple ? props.selectedData : [props.selectedData];
+  }
+
   render() {
-    const { data, onSuccess, selectedData, displayField } = this.props;
+    const { data, onSuccess, displayField, multiple } = this.props;
 
     const filteredData = data
+      .filter(item => !this.state.selectedData.find(data => _.isEqual(data, item)))
       .filter(item => (displayField ? item[displayField] : item).toLowerCase().includes(this.state.searchText.toLowerCase()));
 
     return (
       <SafeAreaView style={{flex: 1}}>
+        <View style={{flex: 0}}>
+          <Text style={{color: '#86939e', fontSize: 16, fontWeight: 'bold'}}>Seleccionado</Text>
+          <FlatList
+            data={this.state.selectedData}
+            keyExtractor={(item, index) => index.toString()}
+            keyboardShouldPersistTaps='always'
+            style={{marginBottom: 10, borderColor: 'grey', borderBottomWidth: 1, minHeight: 28, maxHeight: 160}}
+            renderItem={({item}) =>
+              <View style={{padding: 2, width: '100%'}}>
+                <Text
+                  onPress={() => multiple ? this.setState({
+                      selectedData: this.state.selectedData
+                        .filter(data => data !== item)
+                    }): onSuccess(undefined)}
+                  style={{
+                    fontSize: 18,
+                    borderWidth: 1,
+                    borderColor: 'cyan',
+                    borderRadius: 6,
+                    padding: 5
+                  }}>
+                  {displayField ? item[displayField] : item}
+                </Text>
+              </View>
+            }/>
+        </View>
         <Text style={{color: '#86939e', fontSize: 16, fontWeight: 'bold', flex: 0}}>Selecione</Text>
         <FlatList
           data={filteredData}
@@ -33,15 +74,14 @@ class ListControlModal extends React.Component<ListControlModalProps> {
             const regexpResult = RegExp(
               `^(.*)(${this.state.searchText.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')})(.*)$`, 'i'
             ).exec(displayField ? item[displayField] : item) || [];
-            const selected: boolean = (displayField ? item[displayField] : item) === selectedData;
 
             return <View style={{padding: 2, width: '100%'}}>
               <Text
-                onPress={() => onSuccess(item)}
+                onPress={() => multiple ? this.setState({selectedData: [...this.state.selectedData, item]}) : onSuccess(item)}
                 style={{
                   fontSize: 18,
                   borderWidth: 1,
-                  borderColor: selected ? 'cyan' : 'grey',
+                  borderColor: 'grey',
                   borderRadius: 6,
                   padding: 5
                 }}>
@@ -51,10 +91,17 @@ class ListControlModal extends React.Component<ListControlModalProps> {
               </Text>
             </View>
           }}/>
-        <TextInput style={{flex: 0}}
-          autoFocus={true}
-          value={this.state.searchText}
-          onChangeText={(value) => this.setState({searchText: value})}/>
+        <View style={{flex: 0, flexDirection: 'row'}}>
+          <TextInput
+            style={{flex: 1}}
+            autoFocus={true}
+            value={this.state.searchText}
+            onChangeText={(value) => this.setState({searchText: value})}
+            />
+          <Icon name='check-circle' type= 'material' color='green'
+            size={multiple ? 32 : 0}
+            onPress={() => onSuccess(this.state.selectedData.length > 0 ? this.state.selectedData : undefined)}/>
+        </View>
       </SafeAreaView>
     );
   }
@@ -70,6 +117,14 @@ export default class ListControl extends ControlComponent {
   render() {
     const { controlBridge, navigation } = this.props;
 
+    let selectedValues: Array<any> | undefined;
+    if (controlBridge.OutputValue)
+      selectedValues = controlBridge.property('multiple') ? controlBridge.OutputValue : [controlBridge.OutputValue];
+    else
+      selectedValues = undefined;
+
+
+
      return (
       <View style={{width: '100%', paddingHorizontal: 10, paddingBottom: 25}}>
         <Text
@@ -84,9 +139,13 @@ export default class ListControl extends ControlComponent {
               onSuccess={(output) => { controlBridge.OutputValue = output; navigation.goBack()}}
               selectedData={controlBridge.OutputValue}
               displayField={controlBridge.property('displayfield')}
+              multiple={controlBridge.property('multiple')}/>
           )}
           style={{fontSize: 18, minHeight: 40, borderColor: '#0000001F', borderBottomWidth: 1, paddingVertical: 5}}>
-          {(controlBridge.property('displayfield') ? controlBridge.OutputValue?.[controlBridge.property('displayfield')] : controlBridge.OutputValue) || controlBridge.property('placeholder') || ''}
+          {selectedValues
+            ?.map(selectedValue => (controlBridge.property('displayfield') ? selectedValue[controlBridge.property('displayfield')] : selectedValue))
+            .join('\n') || controlBridge.property('placeholder') || ''}
+          {/* {(controlBridge.property('displayfield') ? controlBridge.OutputValue?.[controlBridge.property('displayfield')] : controlBridge.OutputValue) || controlBridge.property('placeholder') || ''} */}
         </Text>
       </View>
     );
