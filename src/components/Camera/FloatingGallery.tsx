0f81917de2c029e21efe8e50b9e1e920c2c9c8b9
this.fabio <this.fabio@outlook.cl>
2021-09-27 01:03:02 -0300

    Se agrega galería y se optimiza código - Se deja paradigma funcional por la sobrecarga de recursos

--- src/components/Camera/FloatingGallery.tsx
+++ src/components/Camera/FloatingGallery.tsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import React, { FC, useCallback, useState } from 'react';
+import { FlatList, ListRenderItemInfo, StyleSheet, View } from 'react-native';
 import CameraRoll, { GetPhotosParams, PhotoIdentifier } from '@react-native-community/cameraroll';
-import Galleryframe from './GalleryFrame';
+import GalleryFrame from './GalleryFrame';
 
 export interface FloatingGalleryPressItemEvent {
   pressedItemUri: string;
@@ -12,56 +12,60 @@ type Props = {
   selectedItemsUri?: Array<string>;
 };
 
-export default class FloatingGallery extends Component<Props> {
-  state = {
-    photoIdentifiers: new Array<PhotoIdentifier>()
+const FloatingGallery: FC<Props> = (props: Props) => {
+  const [photoIdentifiersState, setPhotoIdentifiersState] = useState<Array<PhotoIdentifier>>([]);
+
+  _getGallery()
+    .then(response => {
+      setPhotoIdentifiersState(response);
+    })
+    .catch(error => console.warn(error));
+
+  const keyExtractorCallback = useCallback((item, index) => index.toString(), []);
+
+  const renderItemCallback = useCallback(({item}: ListRenderItemInfo<PhotoIdentifier>) =>
+    <GalleryFrame
+      imageUri={item.node.image.uri}
+      isVideo={item.node.type.includes('video')}
+      selected={props.selectedItemsUri?.includes(item.node.image.uri)}
+      onPress={() => props.onPressItem?.({pressedItemUri: item.node.image.uri})}
+      size={80}
+    />, [props.selectedItemsUri])
+
+  return (
+    <View style={styles.floatingContainer}>
+      <FlatList
+        data={photoIdentifiersState}
+        keyExtractor={keyExtractorCallback}
+        horizontal={true}
+        maxToRenderPerBatch={8}
+        renderItem={renderItemCallback}/>
+    </View>
+  );
+}
+
+const _getGallery: () => Promise<Array<PhotoIdentifier>> = async () => {
+  let CameraRollOptions: GetPhotosParams = {
+    first: 10
   };
+  let photoIdentifiers: Array<PhotoIdentifier> = [];
+  let nextPage: boolean = true;
 
-  private _getGallery: () => Promise<Array<PhotoIdentifier>> = async () => {
-    let CameraRollOptions: GetPhotosParams = {
-      first: 10
-    };
-    let photoIdentifiers: Array<PhotoIdentifier> = [];
-    let nextPage: boolean = true;
+  while (nextPage) {
+    const photoIdentifiersPage = await CameraRoll.getPhotos(CameraRollOptions);
+    photoIdentifiers.push(...photoIdentifiersPage.edges);
 
-    while (nextPage) {
-      const photoIdentifiersPage = await CameraRoll.getPhotos(CameraRollOptions);
-      photoIdentifiers.push(...photoIdentifiersPage.edges);
-
-      if (photoIdentifiersPage.page_info.has_next_page) {
-        CameraRollOptions = {
-          ...CameraRollOptions,
-          after: photoIdentifiersPage.page_info.end_cursor
-        };
-      }
-
-      nextPage = photoIdentifiersPage.page_info.has_next_page;
+    if (photoIdentifiersPage.page_info.has_next_page) {
+      CameraRollOptions = {
+        ...CameraRollOptions,
+        after: photoIdentifiersPage.page_info.end_cursor
+      };
     }
 
-    return photoIdentifiers;
+    nextPage = photoIdentifiersPage.page_info.has_next_page;
   }
 
-  render() {
-    this._getGallery()
-      .then(response => this.setState({photoIdentifiers: response}))
-      .catch(error => console.warn(error));
-
-    return (
-      <View style={styles.floatingContainer}>
-        <FlatList
-          data={this.state.photoIdentifiers}
-          keyExtractor={(item, index) => index.toString()}
-          horizontal={true}
-          maxToRenderPerBatch={8}
-          renderItem={({item}) =>
-            <Galleryframe photoIdentifier={item}
-              selected={this.props.selectedItemsUri?.includes(item.node.image.uri)}
-              onPress={() => this.props.onPressItem?.({pressedItemUri: item.node.image.uri})}
-            />
-          }/>
-      </View>
-    );
-  }
+  return photoIdentifiers;
 }
 
 const styles = StyleSheet.create({
@@ -71,3 +75,5 @@ const styles = StyleSheet.create({
     width: '100%'
   }
 });
+
+export default FloatingGallery;
--- src/components/Camera/GalleryFrame.tsx
+++ src/components/Camera/GalleryFrame.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import {
   GestureResponderEvent,
   Image,
@@ -7,53 +7,54 @@ import {
   View
 } from 'react-native';
 import { Icon } from 'react-native-elements';
-import { PhotoIdentifier } from '@react-native-community/cameraroll';
 
 type Props = {
   onPress?: ((event: GestureResponderEvent) => void) | null | undefined;
-  photoIdentifier: PhotoIdentifier;
+  imageUri: string;
+  isVideo?: boolean;
   selected?: boolean;
+  size: number;
 };
 
-export default class Galleryframe extends Component<Props> {
-  private _onPress = (event: GestureResponderEvent) => this.props.onPress?.(event);
+const GalleryFrame: FC<Props> = (props: Props) => {
+  const _onPress = (event: GestureResponderEvent) => props.onPress?.(event);
 
-  render() {
-    let videoIcon: JSX.Element | undefined = undefined;
-    if (this.props.photoIdentifier.node.type.includes('video')) {
-      videoIcon =
-        <View style={styles.galleryFrameVideoIcon}>
-          <Icon type='material' name='videocam' color='white'/>
-        </View>
-    }
-
-    return (
-      <View style={styles.galleryFrame}>
-        <Pressable
-          style={[{flex: 1}, this.props.selected ? styles.pressedGalleryFrame : undefined]}
-          onPress={(event) => this._onPress(event)}
-        >
-          <Image source={{uri: this.props.photoIdentifier.node.image.uri}} style={{flex: 1}}/>
-          {videoIcon}
-        </Pressable>
+  let videoIcon: JSX.Element | undefined = undefined;
+  if (props.isVideo) {
+    videoIcon =
+      <View style={styles.galleryFrameVideoIcon}>
+        <Icon type='material' name='videocam' color='white'/>
       </View>
-    );
   }
+
+  return (
+    <View style={{
+      height: props.size,
+      width: props.size-props.size/20,
+      padding: props.size/20
+    }}>
+      <Pressable
+        style={[{flex: 1}, props.selected ? styles.pressedGalleryFrame : undefined]}
+        onPress={(event) => _onPress(event)}
+      >
+        <Image source={{uri: props.imageUri}} style={{flex: 1}}
+          testID={'thumbnail-image-background'}/>
+        {videoIcon}
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  galleryFrame: {
-    height: 80,
-    width: 80,
-    paddingHorizontal: 4
-  },
   galleryFrameVideoIcon: {
     bottom: 0,
     left: 1,
     position: 'absolute'
   },
   pressedGalleryFrame: {
-    borderColor: 'orange',
+    borderColor: '#FDAE01',
     borderWidth: 2
   }
 });
+
+export default GalleryFrame;
