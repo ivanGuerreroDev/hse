320ee7669ade0e6deaf19bd216061f3f87c8859b
this.fabio <this.fabio@outlook.cl>
2021-10-11 20:31:47 -0300

    Se agrega monitor de geolocalización

---
+++ src/utils/geolocation.ts
@@ -0,0 +1,27 @@
+import Geolocation, { GeolocationError, GeolocationOptions, GeolocationResponse } from '@react-native-community/geolocation';
+import { RESULTS } from 'react-native-permissions';
+import { updateGeolocation } from 'state/settings/actions';
+import { store } from 'state/store/store';
+import { checkLocationPermission } from './permissions';
+
+checkLocationPermission()
+  .then(result => {
+    if (result === RESULTS.GRANTED || result === RESULTS.LIMITED) {
+      const positionCallback = (position: GeolocationResponse) => {
+        store.dispatch(updateGeolocation(position));
+      };
+
+      const errorCallback = (error: GeolocationError) => {
+        console.warn(error.message);
+      };
+
+      const options: GeolocationOptions = {
+        timeout: 15 * 1000, //ms: seconds * 1000,
+        maximumAge: 3 * 60000, //ms: minutes * 60000
+        enableHighAccuracy: true,
+      };
+
+      Geolocation.watchPosition(positionCallback, errorCallback, options);
+    }
+  });
+
